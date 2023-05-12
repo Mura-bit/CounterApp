64a1894b32b9eff99436bc12c2a6669ac78c40e5
Murat.bit <mura.usa11@gmail.com>
2023-05-11 17:05:48 -0700

    changedSize

--- src/components/counterButton/CounterButton.jsx
+++ src/components/counterButton/CounterButton.jsx
@@ -18,19 +18,35 @@ const CounterButton = ({ type }) => {
       if (count <= 140) {
         setCount(count - count);
       }
-    };
+  };
+  const multiHandler = () => {
+    if (count <= 140) {
+      setCount(count + 5);
+    }
+  };
+  const devideHandler = () => {
+    if (count <= 140) {
+      setCount(count - 5);
+    }
+  };
     return (
       <section>
         <h3>{count}</h3>
         <div className="main-btn">
           <button className={`${type}`} onClick={coundHandler}>
-            +
+            + 10
           </button>
           <button className={`${type}`} onClick={minusHandler}>
-            -
+            - 10
           </button>
           <button className={`${type}`} onClick={resetHandler}>
             Reset
+          </button>
+          <button className={`${type}`} onClick={multiHandler}>
+            + 5
+          </button>
+          <button className={`${type}`} onClick={devideHandler}>
+            - 5
           </button>
         </div>
       </section>
